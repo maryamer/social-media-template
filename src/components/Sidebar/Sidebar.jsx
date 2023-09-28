957b91f9247047550e9c6d281abaddd2f75643ce
maryameb <personalmaryamebrahimi@gmail.com>
2023-09-29 01:50:15 +0330

    sidebar responsive

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,14 @@
 import { NavLink } from "react-router-dom";
+import {
+  AiOutlineHome,
+  AiOutlineSearch,
+  AiOutlineMessage,
+  AiOutlineHeart,
+  AiOutlinePlusCircle,
+  AiOutlineMenu,
+} from "react-icons/ai";
+import { MdOutlineExplore } from "react-icons/md";
+import { BsPerson } from "react-icons/bs";
 
 function Sidebar() {
   const menuItems = [
@@ -6,15 +16,22 @@ function Sidebar() {
     { title: "Search", id: 2, direction: "/" },
     { title: "Explore", id: 3, direction: "/" },
     { title: "Messages", id: 4, direction: "/" },
-    { title: "Notfications", id: 5, direction: "/" },
+    { title: "Notifications", id: 5, direction: "/" },
     { title: "Create", id: 6, direction: "/" },
     { title: "Profile", id: 7, direction: "/" },
   ];
   return (
-    <div className="sidebar h-14 w-full md:h-full md:w-1/6">
-      <div className="userInfo"></div>
-      <div className="sidebarMenu">
-        <div className="sidebarList">
+    <div className="sidebar  w-full md:h-full md:w-1/6 dark:bg-black  md:relative fixed">
+      <div className="userInfo  hidden md:flex  dark:bg-black ">
+        <span className="sidebarListItem flex md:py-5 md:pl-5 md:pr-1 font-bold">
+          <BsPerson className="w-8 h-8 dark:text-white " />
+        </span>
+        <span className="sidebarListItemText items-center hidden md:flex font-bold dark:text-white">
+          Maryam Ebrahimi
+        </span>
+      </div>
+      <div className="sidebarMenu md:h-3/5  flex-col  dark:bg-black ">
+        <div className="sidebarList overflow-hidden h-14 md:h-full flex md:flex-col justify-around items-center md:items-start ">
           {menuItems.map((item) => (
             <MenuItem
               title={item.title}
@@ -23,13 +40,12 @@ function Sidebar() {
             />
           ))}
         </div>
-        <button className="sidebarButton">Show More</button>
-        <hr className="sidebarHr" />
-        <ul className="sidebarFriendList">
-          {/* {Users.map((u) => (
-            <CloseFriend key={u.id} user={u} />
-          ))} */}
-        </ul>
+        <div className="sidebarListItemText items-center hidden md:flex font-bold absolute bottom-0 w-full  bg-black dark:text-white">
+          <div className="sidebarListItem flex md:p-5 font-bold">
+            <AiOutlineMenu className="w-8 h-8 dark:text-white " />
+          </div>
+          More
+        </div>
       </div>
     </div>
   );
@@ -39,23 +55,26 @@ export default Sidebar;
 
 function MenuItem({ title, direction = "" }) {
   return (
-    <div className="sidebarListItem">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="w-6 h-6"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-        />
-      </svg>
+    <div className="sidebarListItem flex md:p-5 font-bold">
+      {title == "Home" ? (
+        <AiOutlineHome className="w-8 h-8 dark:text-white" />
+      ) : title === "Search" ? (
+        <AiOutlineSearch className="w-8 h-8 dark:text-white" />
+      ) : title === "Explore" ? (
+        <MdOutlineExplore className="w-8 h-8 dark:text-white" />
+      ) : title === "Messages" ? (
+        <AiOutlineMessage className="w-8 h-8 dark:text-white" />
+      ) : title === "Notifications" ? (
+        <AiOutlineHeart className="w-8 h-8 dark:text-white" />
+      ) : title === "Create" ? (
+        <AiOutlinePlusCircle className="w-8 h-8 dark:text-white" />
+      ) : (
+        <BsPerson className="w-8 h-8 dark:text-white" />
+      )}
 
-      <span className="sidebarListItemText">{title}</span>
+      <span className="sidebarListItemText items-center hidden md:flex font-bold dark:text-white">
+        &nbsp;&nbsp;{title}
+      </span>
     </div>
   );
 }
