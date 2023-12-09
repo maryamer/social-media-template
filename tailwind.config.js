0fb59f2560df682c09b0487a9ff3fe5657da398a
maryam <personalmaryamebrahimi@gmail.com>
2023-12-09 18:29:40 +0330

    commit for deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class",
-  theme: {
-    extend: {
-      scrollbar: ["rounded", "dark"],
-      width: {
-        "1/7": "16%",
-        "95%": "95%",
-        200: "890px",
-      },
-      height: {
-        "8/10": "80%",
-        "8/100": "8%",
-      },
-      animation: {
-        loading: {
-          "0%, 100%": { transform: "rotate(-3deg)" },
-          "50%": { transform: "rotate(3deg)" },
-        },
+  // darkMode: "class",
+  // theme: {
+  //   extend: {
+  //     scrollbar: ["rounded", "dark"],
+  //     width: {
+  //       "1/7": "16%",
+  //       "95%": "95%",
+  //       200: "890px",
+  //     },
+  //     height: {
+  //       "8/10": "80%",
+  //       "8/100": "8%",
+  //     },
+  //     animation: {
+  //       loading: {
+  //         "0%, 100%": { transform: "rotate(-3deg)" },
+  //         "50%": { transform: "rotate(3deg)" },
+  //       },
 
-        wiggle: "wiggle 1s ease-in-out infinite",
-      },
-    },
-  },
-  variants: {},
+  //       wiggle: "wiggle 1s ease-in-out infinite",
+  //     },
+  //   },
+  // },
+  // variants: {},
   plugins: [require("tailwind-scrollbar")],
 };
